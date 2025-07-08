ad2a8172344d5ba73d59bd41f0f3826c836a9cd3
Francesco <>
2025-07-08 14:38:45 +0200

    microservizio Budget

---
+++ Budget-service/Budget/src/main/java/Budget/Budget/ServletInitializer.java
@@ -0,0 +1,13 @@
+package Budget.Budget;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BudgetApplication.class);
+	}
+
+}
